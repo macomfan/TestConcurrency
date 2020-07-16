ab236b72c198a9c977e1da73b0c0ac811c695f8b
Wenrui <macomfan@163.com>
2020-07-16 21:12:44 +0800

    add mcs lock, problem if thread num > core num

--- Lock.h
+++ Lock.h
@@ -360,4 +360,96 @@ namespace L {
     private:
         ngx_atomic_t lock = 0;
     };
+
+#define cmpxchg(P, O, N) __sync_val_compare_and_swap((P), (O), (N))
+
+#define barrier() asm volatile("": : :"memory")
+#define cpu_relax() asm volatile("pause\n": : :"memory")
+
+    static inline void *xchg_64(void *ptr, void *x) {
+        __asm__ __volatile__("xchgq %0,%1"
+                : "=r" ((unsigned long long) x)
+                : "m" (*(volatile long long *) ptr), "0" ((unsigned long long) x)
+                : "memory");
+
+        return x;
+    }
+
+    typedef struct mcs_lock_t mcs_lock_t;
+
+    struct mcs_lock_t {
+        mcs_lock_t *next;
+        int spin;
+    };
+    typedef struct mcs_lock_t *mcs_lock;
+
+    static inline void lock_mcs(mcs_lock *m, mcs_lock_t *me) {
+        mcs_lock_t *tail;
+
+        me->next = NULL;
+        me->spin = 0;
+
+        tail = (mcs_lock_t*)xchg_64(m, me);
+
+        /* No one there? */
+        if (!tail) return;
+
+        /* Someone there, need to link in */
+        tail->next = me;
+
+        /* Make sure we do the above setting of next. */
+        barrier();
+
+        /* Spin on my spin variable */
+        while (!me->spin) cpu_relax();
+
+        return;
+    }
+
+    static inline void unlock_mcs(mcs_lock *m, mcs_lock_t *me) {
+        /* No successor yet? */
+        if (!me->next) {
+            /* Try to atomically unlock */
+            if (cmpxchg(m, me, NULL) == me) return;
+
+            /* Wait for successor to appear */
+            while (!me->next) cpu_relax();
+        }
+
+        /* Unlock next one */
+        me->next->spin = 1;
+    }
+
+    static inline int trylock_mcs(mcs_lock *m, mcs_lock_t *me) {
+        mcs_lock_t *tail;
+
+        me->next = NULL;
+        me->spin = 0;
+
+        /* Try to lock */
+        tail = cmpxchg(m, NULL, &me);
+
+        /* No one was there - can quickly return */
+        if (!tail) return 0;
+
+        return 1; // Busy
+    }
+
+    class MCSLock {
+    public:
+
+        void lock() {
+            lock_mcs(&cnt_lock, &local_lock);
+        }
+
+        void unlock() {
+            unlock_mcs(&cnt_lock, &local_lock);
+        }
+
+    private:
+        mcs_lock cnt_lock = NULL;
+        static thread_local mcs_lock_t local_lock;
+    };
+    
+    thread_local mcs_lock_t MCSLock::local_lock;
 }
